701b10b508064aeedf67cbb06f194a4ebc3843bf
Lizandro Antonio <codeversebackend@gmail.com>
2024-01-09 18:52:54 -0600

    Link About me

--- js/link.js
+++ js/link.js
@@ -1,6 +1,4 @@
 function redirectToUrl(button) {
-    // Lee la URL desde el atributo data-href
     var url = button.getAttribute('data-href');
-    // Redirige a la URL especificada
     window.location.href = url;
 }
